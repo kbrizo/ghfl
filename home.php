0a97aebbec0c28cdd84dd851c9f156ae674740d4
Kayla Brizo <kbrizo+riwi@gmail.com>
2018-05-19 14:27:34 -0700

    still trying to fix the form layout

--- home.php
+++ home.php
@@ -108,10 +108,10 @@ wp_reset_postdata();?>
 <h1>Sign Up for Updates from The Lab</h1>
 
 <form action="https://brizofalltrades.us18.list-manage.com/subscribe/post?u=e04195f323346e4c0f2b4791a&amp;id=748982ef44" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-	<p><input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Your Email Address" required></p>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Your Email Address" required>
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e04195f323346e4c0f2b4791a_748982ef44" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
 </form>
 </div>
 
